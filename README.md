7501f1424db55957d05024013dc935278cbdc320
rducky <sakeofmaking@gmail.com>
2025-06-07 10:26:33 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Countdown
+
+# Ref
+1. [Link to Site](https://sakeofmaking.github.io/countdown2-page/)
+
+### Description
+* Countdown timer to retirement
+* Date is entered in format YYYY-MM-DDThh:mm:ss
+
---
+++ script.js
@@ -0,0 +1,34 @@
+// @source: https://stackoverflow.com/questions/41145086/js-countdown-timer-to-a-specific-date
+
+$(document).ready(function () {
+    var countTo = $('#count_to').text();
+    var deadline = new Date(countTo);
+
+    function updateClock() {
+        var now = new Date();
+        var t = deadline - now;
+
+        if (t <= 0) {
+            $('#days').text('00');
+            $('#hours').text('00');
+            $('#minutes').text('00');
+            $('#seconds').text('00');
+            $('#message').text('SEE YOU LATER!');
+            clearInterval(timeinterval);
+            return;
+        }
+
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+        $('.days').text(('0' + days).slice(-2));
+        $('.hours').text(('0' + hours).slice(-2));
+        $('.minutes').text(('0' + minutes).slice(-2));
+        $('.seconds').text(('0' + seconds).slice(-2));
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+});
